4076cebc3f0f98b8e3f7f727aa884e9809e42d8a
Androgymo <androgymo@gmail.com>
2017-02-12 13:27:23 -0500

    Iniitial Commit

---
+++ script.js
@@ -0,0 +1,19 @@
+var quote = new Array();
+quote[0] = "\“There would have been nothin’ left to do for someone less astute. He would’ve been dead or destitute without a cent of restitution. Started workin’, clerkin’ for his late mother’s landlord, tradin’ sugarcane and rum and all the things he can’t afford. Scammin’ for every book he can get his hands on, plannin’ for the future. See him now as he stands on the bow of a ship headed for a new land. In New York you can be a new man.\” —Aaron Burr, \“Alexander Hamilton\” ";
+quote[1] = "\“I’m just like my country—I’m young, scrappy, and hungry, and I am not throwing away my shot.\” —Hamilton, \“My Shot\”";
+quote[2] = "\“The plan is to fan this spark into a flame.\” —Hamilton, \“My Shot\”";
+quote[3] ="\“I never had a group of friends before, I promise that I’ll make y’all proud.\” —Hamilton, \“My Shot\”";
+quote[4] = "\“I’m past patiently waitin’ I’m passionately smashin’ every expectation. Every action’s an act of creation!\” —Hamilton, \“My Shot\”";
+quote[5] ="\“I may not live to see our glory, but I will gladly join the fight. And when our children tell our story, they’ll tell the story of tonight.\” —Hamilton, \“The Story of Tonight\”";
+quote[6] = "\“You want a revolution? I want a revelation. So listen to my declaration: \“We hold these truths to be self-evident that all men are created equal.\” And when I meet Thomas Jefferson, I’m ‘a compel him to include women in the sequel!\” —Angelica Schuyler, \“The Schuyler Sisters\”"
+quote[7] = "\"Look around, look around at how lucky we are to be alive right now! History is happening in Manhattan and we just happen to be in the greatest city in the world.\” —The Schuyler Sisters, \“The Schuyler Sisters\”";
+quote[8] = "\"Burr I’d rather be divisive than indecisive. Drop the niceties.\” —Hamilton, \“A Farmer Refuted\”";
+quote[9] = "\"There’s a million things I haven’t done. Just you wait, just you wait.\” —Hamilton, \“Satisfied\”";
+quote[10] = "\"How does a ragtag volunteer army in need of a shower somehow defeat a global superpower? How do we emerge victorious from the quagmire? Leave the battlefield waving Betsy Ross’ flag higher? Yo. Turns out we have a secret weapon, an immigrant you know and love who’s unafraid to step in. He’s constantly confusin’, confoundin’ the British henchmen— ev’ryone give it up for America’s favorite fighting Frenchman! Lafayette!\” —Burr, \“Guns and Ships\”";
+quote[11] = "\"I know that we can win, I know that greatness lies in you. But remember from here on in, history has its eyes on you.\” —Washington, \“History Has Its Eyes on You\”"
+quote[12] = "\"We’re finally on the field, we’ve had quite a run. Immigrants: we get the job done.\” —Hamilton and Lafayette, \“Yorktown\”";
+
+
+var randomQuote = quote[Math.floor(Math.random() * quote.length)];
+
+console.log(randomQuote);
